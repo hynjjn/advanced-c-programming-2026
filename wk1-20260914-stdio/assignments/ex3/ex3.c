64b2afa59749919e6be47474e310104a2c6d7357
hynjjn <hynjjn@proton.me>
2026-09-15 10:16:07 +0900

    0914

---
+++ wk1-20260914-stdio/assignments/ex3/ex3.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    system("title 세제곱과 나눗셈 연산");
+
+    printf("2022322020 선현진\n");
+
+    int num1, num2, triple;
+    float division;
+
+    printf("정수값 2개 입력 : ");
+    scanf_s("%d %d", &num1, &num2);
+
+    triple = num1 * num1 * num1;
+    division = (float)num1 / num2;
+
+    printf("세제곱 연산 결과 : %d * %d * %d = %08d\n", num1, num1, num1, triple);
+    printf("나눗셈 연산 결과 : %d / %d = %08.3f\n", num1, num2, division);
+
+    return 0;
+}
---
+++ wk1-20260914-stdio/assignments/ex4/ex4.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    system("title 알파벳과 아스키코드값");
+
+    printf("2022322020 선현진\n");
+
+    char alpha;
+
+    printf("알파벳 입력 : ");
+    scanf_s("%c", &alpha, 1);
+
+    printf("입력한 알파벳 : %c\n", alpha);
+    printf("알파벳 아스키코드값 : %d\n", alpha);
+    printf("입력한 알파벳 + 8 : %c\n", alpha + 8);
+    printf("프로그램을 종료합니다.\n");
+
+    return 0;
+}
